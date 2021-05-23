a4787bb142cc2f16ac64fd4ad5c87f5ba393050d
mahendraetampawala <mahendraetampawala98@gmail.com>
2021-05-23 13:27:09 +0530

    Initial Commit

--- Inspiring_Future_Mobile_Repairing_System/src/Client/newClients.java
+++ Inspiring_Future_Mobile_Repairing_System/src/Client/newClients.java
@@ -3,6 +3,11 @@ package Client;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class newClients {
 
@@ -37,7 +42,41 @@ public class newClients {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(30, 30, 1500, 800);
+		frame.setTitle("Mobile Repairing System");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 269, 753);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 51));
+		panel_1.setBounds(10, 10, 247, 38);
+		panel.add(panel_1);
+		
+		JButton btnNewButton_2_2 = new JButton("Clients");
+		btnNewButton_2_2.setForeground(new Color(255, 255, 255));
+		btnNewButton_2_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2_2.setBackground(new Color(51, 0, 153));
+		panel_1.add(btnNewButton_2_2);
+		
+		JButton btnNewButton_2 = new JButton("Jobs");
+		btnNewButton_2.setForeground(new Color(255, 255, 255));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBackground(new Color(51, 0, 153));
+		panel_1.add(btnNewButton_2);
+		
+		JButton btnNewButton_2_1 = new JButton("Advanced");
+		btnNewButton_2_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_2_1.setBackground(new Color(51, 0, 153));
+		panel_1.add(btnNewButton_2_1);
 	}
-
 }
