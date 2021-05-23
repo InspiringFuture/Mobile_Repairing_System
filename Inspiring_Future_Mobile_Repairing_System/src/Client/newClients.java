f5a17946d1c28caa308e66f389bd7707dcd9c8ac
mahendraetampawala <mahendraetampawala98@gmail.com>
2021-05-23 12:55:23 +0530

    updated

---
+++ Inspiring_Future_Mobile_Repairing_System/src/Client/newClients.java
@@ -0,0 +1,43 @@
+package Client;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class newClients {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					newClients window = new newClients();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public newClients() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(30, 30, 1500, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
